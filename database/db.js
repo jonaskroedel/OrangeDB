512052e135cac5f2d899ffca04e3dc46cb3a40a1
Jonas Krödel <jonas.kroedel@gmail.com>
2022-04-05 13:46:38 +0200

    changed database source to aws

--- database/db.js
+++ database/db.js
@@ -2,6 +2,7 @@ require('dotenv').config({path: '../.env'});
 const mysql = require('mysql2/promise');
 
 module.exports = mysql.createConnection({
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
