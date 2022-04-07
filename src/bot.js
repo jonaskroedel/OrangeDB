624ebc691e2f3da38186468a3f0aeae10c06514c
Jonas Krödel <jonas.kroedel@gmail.com>
2022-04-07 20:35:33 +0200

    fix

--- src/bot.js
+++ src/bot.js
@@ -4,7 +4,7 @@ const client = new Client({intents: [Intents.FLAGS.GUILDS, "GUILD_MESSAGES"]});
 const { registerCommands, registerEvents } = require('./utils/register');
 
 (async () => {
-    await client.login(process.env.BOT_TOKEN);
+    lient.login(process.env.BOT_TOKEN);
     client.commands = new Map();
     await registerCommands(client, '../commands');
     await registerEvents(client, '../events');
