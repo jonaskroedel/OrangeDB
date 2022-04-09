d143bd525ddc4c19d70a1cc527d00a2cc2b08d03
Jonas Krödel <jonas.kroedel@gmail.com>
2022-04-09 18:48:28 +0200

    set right path

--- src/bot.js
+++ src/bot.js
@@ -1,11 +1,11 @@
-require('dotenv').config({path: '../.env'});
+require('dotenv').config();
 const {Client, Intents} = require('discord.js');
 const client = new Client({intents: [Intents.FLAGS.GUILDS, "GUILD_MESSAGES"]});
 
 const {registerCommands, registerEvents} = require('./utils/register');
 
 (async () => {
-    await client.login(process.env.BOT_TOKEN);
+    client.login(process.env.BOT_TOKEN);
     client.commands = new Map();
     await registerCommands(client, '../commands');
     await registerEvents(client, '../events');
