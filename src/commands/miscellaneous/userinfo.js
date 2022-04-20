d300e14950abbad475b68051fabc033ce1267f4e
Jonas Krödel <jokroedel@tsn.at>
2022-04-20 18:01:41 +0200

    userinfo

--- src/commands/miscellaneous/userinfo.js
+++ src/commands/miscellaneous/userinfo.js
@@ -1,8 +1,10 @@
-const {Client, Intents, MessageEmbed} = require('discord.js');
+const {Client, Intents, MessageEmbed, Formatters} = require('discord.js');
 const client = new Client({intents: [Intents.FLAGS.GUILDS, "GUILD_MESSAGES"]});
 const BaseCommand = require("../../utils/structures/BaseCommand");
 const StateManager = require("../../utils/StateManager");
 const moment = require('moment');
+const rtf1 = new Intl.RelativeTimeFormat('en', { style: 'narrow'});
+
 
 module.exports = class help extends BaseCommand {
     constructor() {
@@ -17,16 +19,24 @@ module.exports = class help extends BaseCommand {
         let member = message.mentions.members.first() || message.member,
             user = member.user;
 
+        const diff = moment.duration(moment.utc().diff(user.createdTimestamp));
+        let age = `${diff.years()} years ${diff.months()} months ${diff.days()} and days`;
+
         const sEmbed = new MessageEmbed()
-            .setColor("#ff1966")
+            .setColor(member.roles.color.hexColor)
             .setTitle(`Userinfo for ${user.username}`)
             .setThumbnail(user.avatarURL())
+            .addField(`Username:`, user.username, true)
+            .addField(`Discriminator:`, user.discriminator, true)
+
+            .addField(`Account age:`, age, false)
+
+
             .setDescription(`
                 Username: \`${user.username}\` Discriminator: \`#${user.discriminator}\`
-                Since **${moment.utc(member.joinedAt).format('DD.MM.YY')}** on \`${message.guild}\`
-                Account created on **${moment.utc(user.createdAt).format('DD.MM.YY')}**
                 ${member.permissions.has("ADMINISTRATOR") ? 'User is `Administrator`' : 'User is `no Administrator`'}
-                ${user.bot ? 'User is a `bot`' : 'User is `not a bot`'}`)
+                ${user.bot ? 'User is a `bot`' : 'User is `not a bot`'}
+                Roles: ${member.roles.highest}`)
             .setFooter({ text:`Requested by ${message.author.username}`, iconURL: message.author.displayAvatarURL({dynamic: true})})
             .setTimestamp()
         message.channel.send({embeds: [sEmbed]});
